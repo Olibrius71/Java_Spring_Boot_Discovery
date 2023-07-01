6bd3ea70d4dfc0be6db8d0943811603137cbdd52
Anis BEN JEMIA <anis.ben-jemia@etu.univ-lyon1.fr>
2023-07-01 22:13:18 +0200

    Implémentation de la page de jeu: titre et progressbar pour le temps restant et de la grille contenant les cartes (cartes importées)

--- src/main/resources/static/js/game.js
+++ src/main/resources/static/js/game.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
 
+    $(".game-time-progress-bar").progressbar({
+        value: 100,
+        classes: {
+            "ui-progressbar-value": "ui-corner-all"
+        }
+    });
+
+    let pbTime = 30;
+    let intervalUpdate = pbTime * 1000 / 100;
+
+    let progBarTimeRunning = setInterval(() => {
+        console.log("loop");
+        let currentPbValue = $(".game-time-progress-bar").progressbar("option", "value");
+        console.log(currentPbValue);
+        $(".game-time-progress-bar").progressbar("option", "value", currentPbValue - 1);
+
+        if (currentPbValue == 0) clearInterval(progBarTimeRunning);
+    },intervalUpdate);
+
 });
